f721e3a2ff82d727fcad9219d3d6dc16f4eaac43
Tristan SWART <tswart@c4r23s3.wethinkcode.co.za>
2019-09-13 10:27:26 +0200

    Update

--- srcs/ft_insertsort.c
+++ srcs/ft_insertsort.c
@@ -1,6 +1,11 @@
 #include "push_swap.h"
 
-int     ft_orderedinsert(t_stack *list)
+int     ft_orderedinsert(t_stack **list)
 {
+    int len;
     
+    if ((len = ft_listlen(list)) == 3)
+    {}
+    else if (len > 3)
+    {}
 }
